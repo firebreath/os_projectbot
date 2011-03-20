d2370e884285716be09573aeeec272e3fcb1725d
Richard Bateman <taxilian@gmail.com>
2011-03-19 19:55:35 -0600

    Fixed duplicate code issue

--- github.py
+++ github.py
@@ -8,6 +8,7 @@ Written for use in the #firebreath IRC channel: http://www.firebreath.org
 """
 
 from github2.client import Github
+from os_projectbot.shorten import shorten
 import re
 import web
 
@@ -107,29 +108,3 @@ f_find_github_tag.thread = True
 # use Google's http://goo.gl/ URL shortener
 # requires urllib, urllib2, re, simplejson
 # This is shamelessly borrowed from http://segfault.in/2010/10/shorten-urls-using-google-and-python/
-def shorten(url):
-    try:
-        from re import match
-        from urllib2 import urlopen, Request, HTTPError
-        from urllib import quote
-        from json import loads
-    except ImportError, e:
-        raise Exception('Required module missing: %s' % e.args[0])
-    if not match('http://',url) and not match('https://', url):
-        raise Exception('URL must start with "http://"')
-    try:
-        a = urlopen(Request('http://goo.gl/api/url','url=%s'%quote(url),{'User-Agent':'toolbar'}))
-        j = loads(a.read())
-        return j["short_url"]
-    except HTTPError, e:
-        j = loads(e.read())
-        print j
-        if 'short_url' not in j:
-            try:
-                from pprint import pformat
-                j = pformat(j)
-            except ImportError:
-                j = j.__dict__
-            raise Exception('Didn\'t get a correct-looking response. How\'s it look to you?\n\n%s'%j)
-        return j['short_url']
-    raise Exception('Unknown eror forming short URL.')
--- jira.py
+++ jira.py
@@ -10,6 +10,7 @@ http://inamidst.com/phenny/
 import xmlrpclib
 import time
 import re
+from os_projectbot.shorten import shorten
 #import modules.activecollab
 
 recent_tickets = {}
@@ -120,35 +121,4 @@ def setup(self):
 
 
 
-
-#!/usr/bin/python
-# use Google's http://goo.gl/ URL shortener
-# requires urllib, urllib2, re, simplejson
-# This is shamelessly borrowed from http://segfault.in/2010/10/shorten-urls-using-google-and-python/
-def shorten(url):
-    try:
-        from re import match
-        from urllib2 import urlopen, Request, HTTPError
-        from urllib import quote
-        from json import loads
-    except ImportError, e:
-        raise Exception('Required module missing: %s' % e.args[0])
-    if not match('http://',url) and not match('https://', url):
-        raise Exception('URL must start with "http://"')
-    try:
-        a = urlopen(Request('http://goo.gl/api/url','url=%s'%quote(url),{'User-Agent':'toolbar'}))
-        j = loads(a.read())
-        return j["short_url"]
-    except HTTPError, e:
-        j = loads(e.read())
-        print j
-        if 'short_url' not in j:
-            try:
-                from pprint import pformat
-                j = pformat(j)
-            except ImportError:
-                j = j.__dict__
-            raise Exception('Didn\'t get a correct-looking response. How\'s it look to you?\n\n%s'%j)
-        return j['short_url']
-    raise Exception('Unknown eror forming short URL.')
 
