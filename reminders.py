a380d9b0009f95c2b571ee27fb9362411131619e
Richard Bateman <taxilian@gmail.com>
2011-06-22 12:02:20 -0600

    Added simple help, shortcut for explaining plugins vs extensions

--- reminders.py
+++ reminders.py
@@ -28,4 +28,29 @@ def lmgt(phenny, input):
     phenny.say("Let me google that for you: %s" % shorten(url))
 lmgt.commands = ['lmgt']
 lmgt.priority = 'high'
-lmgt.example = '.g swhack'
+lmgt.example = '.lmgt swhack'
+
+def bothelp(phenny, input):
+    phenny.say("Bot help -- most used commands:")
+    phenny.say("!findfile <regex> - Search for files in the project tree on github")
+    phenny.say("!find <symbol> - use ctags to search for a symbol in the source tree")
+    phenny.say("!wiki <search string> - search the wiki for the given string")
+    phenny.say("git pull requests - list all open pull requests for the project")
+    phenny.say(".pb - remind users about pastebin services")
+    phenny.say(".ask - remind users to ask their question rather than waiting for someone to come")
+    phenny.say(".g <search string> - search google for <search string> and return the url of the first response")
+    phenny.say(".lmgt <search string> - display 'Let me Google that for you' link for <search string>")
+    phenny.say(".extensions - Display a summary of the difference between a plugin and an extension.")
+
+bothelp.commands = ['bothelp']
+bothelp.priority = "medium"
+
+def extensions(phenny, input):
+    phenny.say("Browser plugins vs extensions: Plugins live in an <object> tag and only interact with the DOM.")
+    phenny.say("They don't hook into any special events with the browser, aren't given special web browser info.")
+    phenny.say("They cannot automatically affect a page without being inserted into said page.")
+    phenny.say("Note that FireBreath plugins can often be used as part of an extension.")
+    phenny.say("For more information about the difference between a plugin and extension, see http://npapi.com/extensions")
+
+extensions.commands = ["extensions"]
+extensions.priority = "medium"
